3a4d9db59679cba9c3a92cc565fbc99740cc3730
Dylan Price <pricedylan001@gmail.com>
2020-02-06 21:28:35 -0500

    Updated my script to allow downloading my SQL

--- app/public/wp-content/themes/Test/db_install.sh
+++ app/public/wp-content/themes/Test/db_install.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
 rm -rf _database/*.zip;
+mkdir _database;
 
-mysqldump -u drp -p local > _database/temp.sql;
-zip _database/temp.zip _database/temp.sql;
+cd _database;
 
-rm -rf _database/temp.sql;
+mysqldump -u drp -p local > temp.sql;
+
+read -p "Old: " old;
+read -p "New: " new;
+
+filename="$(date +'%Y-%m-%d')";
+
+cat temp.sql | sed -e "s@${old}@${new}@g" > ${filename}.sql
+mysql -u drp -p local < ${filename}.sql;
+
+zip ${filename}.zip ${filename}.sql;
+
+rm -rf ${filename}.sql;
+rm -rf temp.sql;
